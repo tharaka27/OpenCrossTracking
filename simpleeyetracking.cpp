b714651a9003e0bac51cd67c5a384fe0e9e51a40
Tharaka Ratnayake <tharakasachintha27@gmail.com>
2022-03-04 22:49:17 +0530

    basic eye tracking completed

--- simpleeyetracking.cpp
+++ simpleeyetracking.cpp
@@ -32,10 +32,6 @@ bool sortbydistance(const cv::Point &first, const cv::Point &second)
 
 void simpleEyeTracking::sortPoints(std::vector<cv::Point> &pointList)
 {
-    //std::sort(pointList.begin(), pointList.end(), sortbyY );
-
-    //std::sort(pointList.begin(), pointList.end(), sortbyX );
-
     std::sort(pointList.begin(), pointList.end(), sortbydistance);
 
     std::sort(pointList.begin()+1, pointList.end()-1, sortbyX);
@@ -70,14 +66,14 @@ void simpleEyeTracking::warpPerspective()
     cv::Mat test = cv::Mat::zeros( 400, 400, CV_8UC3);
 
     cv::circle(test, cv::Point(pupilCenter[0], pupilCenter[1]), 1, cv::Scalar(255,255,255));
-    cv::circle(test, cv::Point(topLeft.x, topLeft.y), 2, cv::Scalar(0,0,255));
-    cv::circle(test, cv::Point(topRight.x, topRight.y), 2, cv::Scalar(255,0,0));
-    cv::circle(test, cv::Point(bottomLeft.x, bottomLeft.y), 2, cv::Scalar(0,255,0));
-    cv::circle(test, cv::Point(bottomRight.x, bottomRight.y), 2, cv::Scalar(255,255,255));
+    cv::circle(test, cv::Point(topLeft.x, topLeft.y), 2, cv::Scalar(0,0,255)); // Red
+    cv::circle(test, cv::Point(topRight.x, topRight.y), 2, cv::Scalar(255,0,0)); // blue
+    cv::circle(test, cv::Point(bottomLeft.x, bottomLeft.y), 2, cv::Scalar(0,255,0)); // green
+    cv::circle(test, cv::Point(bottomRight.x, bottomRight.y), 2, cv::Scalar(255,255,255)); // white
     cv::imshow("polygon", test);
     cv::waitKey(10);
 
-    /*
+
     float widthA = std::sqrt( (bottomRight.x - bottomLeft.x)* (bottomRight.x - bottomLeft.x)
             +  (bottomRight.y - bottomLeft.y) * ( bottomRight.y - bottomLeft.y));
     float widthB = std::sqrt( (topRight.x - topRight.x)*(topRight.x - topRight.x) +
@@ -90,53 +86,20 @@ void simpleEyeTracking::warpPerspective()
             (topLeft.y - bottomLeft.y) * (topLeft.y - bottomLeft.y) );
     float maxHeight = returnMax(heightA, heightB);
 
-
-
-
-    float src_base[4][2] =
-    {
-        {glintPositions[0].x, glintPositions[0].y },
-        {glintPositions[1].x, glintPositions[1].y },
-        {glintPositions[3].x, glintPositions[3].y },
-        {glintPositions[2].x, glintPositions[2].y }
-    };
-
-    float dst_base[4][2] =
-    {
-        {0, 0},
-        {maxWidth -1 , 0 },
-        {maxWidth -1, maxHeight -1},
-        {0, maxHeight - 1}
-    };
-
-    cv::Mat dst = cv::Mat(4, 2, CV_32F, dst_base);
-    //cv::Mat src = cv::Mat(4, 2, CV_32F, src_base);
-
     std::vector<cv::Point2f> scene;
     std::vector<cv::Point2f> object;
 
-    scene.push_back(cv::Point2f(glintPositions[0].x, glintPositions[0].y));
-    scene.push_back(cv::Point2f(glintPositions[1].x, glintPositions[1].y));
-    scene.push_back(cv::Point2f(glintPositions[3].x, glintPositions[3].y));
-    scene.push_back(cv::Point2f(glintPositions[2].x, glintPositions[2].y));
+    scene.push_back(cv::Point2f(topLeft.x, topLeft.y));
+    scene.push_back(cv::Point2f(topRight.x, topRight.y));
+    scene.push_back(cv::Point2f(bottomRight.x, bottomRight.y));
+    scene.push_back(cv::Point2f(bottomLeft.x, bottomLeft.y));
 
     object.push_back(cv::Point2f( 0, 0 ));
     object.push_back(cv::Point2f( maxWidth -1 , 0  ));
     object.push_back(cv::Point2f( maxWidth -1, maxHeight -1 ));
     object.push_back(cv::Point2f( 0, maxHeight - 1 ));
-    //std::cout << dst << std::endl;
-    //std::cout << rect << std::endl;
-    std::cout << "Max width " << maxWidth << std::endl;
-    std::cout << "Max height " << maxHeight << std::endl;
 
-    //cv::Mat M = cv::getPerspectiveTransform(rect, dst);
-
-    //std::cout << src << std::endl;
-    //std::cout << M << std::endl;
-    //std::cout << dst << std::endl;
-
-    //cv::Mat prediction = cv::Mat(1, 3, CV_64F, prediction_base);
-    cv::Mat warped;
+    //cv::Mat warped;
 
     cv::Mat mask;
     cv::Mat H;
@@ -160,47 +123,26 @@ void simpleEyeTracking::warpPerspective()
     gazePoint.y = prediction[0].y;
 
 
-
     std::cout << "----------------->prediction is" << prediction[0].x <<"," << prediction[0].y << std::endl;
 
-    const int rows = 720;
-    const int cols = 1280;
-    /*
+    const int rows = 1024;  // 720
+    const int cols = 1280; // 1280
 
-    cv::Mat test = cv::Mat::zeros(rows, cols , CV_8UC1);
-    std::string p = "x:" + std::to_string(gazePoint.x)
-            + " y:" + std::to_string(gazePoint.y);
+    cv::Mat test2 = cv::Mat::zeros(rows, cols , CV_8UC1);
 
-    //cv::putText(test, p, cv::Point(100,100), cv::FONT_HERSHEY_COMPLEX, 1, cv::Scalar(255, 0,0));
-    gazePoint.x = rows - (gazePoint.x / maxWidth * rows);
-    gazePoint.y = gazePoint.y / maxHeight * cols;
-    cv::circle(test, gazePoint, 3, cv::Scalar(255,0,0));
-    cv::imshow("datapoints", test );
+    gazePoint.x = cols - gazePoint.x / maxWidth * cols;
+    gazePoint.y = gazePoint.y / maxHeight * rows ;
+    cv::circle(test2, gazePoint, 3, cv::Scalar(255,0,0));
+    cv::imshow("datapoints", test2 );
     cv::waitKey(10);
 
-    cv::Mat test = cv::Mat::zeros(maxHeight, maxWidth, CV_8UC1);
-    cv::circle(test, gazePoint, 3, cv::Scalar(255,0,0));
-    cv::imshow("datapoints", test );
-    cv::waitKey(10);
-    */
 }
 
 void simpleEyeTracking::track()
 {
-    //std::cout << "Heeello world" << std::endl;
 
     // identify four locations
     sortPoints(glintPositions);
-
-    /*
-    for(int i=0; i< glintPositions.size(); i++ )
-    {
-        std::cout << i << "th element is x: "
-                  << glintPositions[i].x
-                  << " y " << glintPositions[i].y
-                  << std::endl;
-    }
-    */
 
     // check whether the eye is outside the polygon
     warpPerspective();
